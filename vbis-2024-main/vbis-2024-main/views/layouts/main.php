0b7317b240cd612cd05ec7d32e7dfc09f2fe2ffb
Nataliyay <natalijajolovic@gmail.com>
2024-11-01 08:17:32 +0100

    Dodajemo na main branch(JEDINO SADA)

---
+++ vbis-2024-main/vbis-2024-main/views/layouts/main.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<html lang="eng">
+<head>
+    <title>layout</title>
+</head>
+
+<body>
+<header style="width: 100%; background-color: red; color: white">
+    HEADER PAGE
+</header>
+
+<main>
+
+    {{ RENDER_SECTION }}
+
+</main>
+
+</body>
+
+<footer>
+    FOOTER PAGE
+</footer>
+
+</html>
